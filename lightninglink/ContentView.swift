bc5d5198a156fe7d94e4ba219ebd933a5595275c
William Casarin <jb55@jb55.com>
2022-01-07 15:07:33 -0800

    Initial Commit

---
+++ lightninglink/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  lightninglink
+//
+//  Created by William Casarin on 2022-01-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ lightninglink/lightninglinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  lightninglinkApp.swift
+//  lightninglink
+//
+//  Created by William Casarin on 2022-01-07.
+//
+
+import SwiftUI
+
+@main
+struct lightninglinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
