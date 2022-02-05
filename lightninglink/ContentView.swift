6d3cf24117a5b718596267b6a371c1622a549117
William Casarin <jb55@jb55.com>
2022-02-05 10:23:35 -0800

    pay qr code scanner
    Signed-off-by: William Casarin <jb55@jb55.com>

--- lightninglink/ContentView.swift
+++ lightninglink/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var info: GetInfo
+    @State private var showingQRScanner = false
 
     init(info: GetInfo) {
         self.info = info
@@ -17,9 +18,17 @@ struct ContentView: View {
     var body: some View {
         let _self = self
         VStack {
+            Button("Pay") {
+                showingQRScanner = true
+            }
             Text(self.info.alias)
             Text("\(self.info.num_active_channels) active channels")
             Text("\(self.info.msatoshi_fees_collected / 1000) sats collected in fees")
+        }
+        .sheet(isPresented: $showingQRScanner) {
+            QRScanner() { code in
+                print(code)
+            }
         }
     }
 }
